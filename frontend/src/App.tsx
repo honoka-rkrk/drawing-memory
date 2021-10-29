5f31d05f967db3cc8e6112986722e9bd93b1f2e1
HONOKA ANRAKU <rakuraku2563nonnon@gmail.com>
2021-10-29 15:38:34 +0900

    init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+import {useState,useEffect} from 'react';
+
+function App() {
+  const [message,setMessage] = useState('');
+
+  useEffect(() => {
+    const apiRequest = () =>  fetch('/api')
+    .then((res) =>res.json())
+    .then((data) => setMessage(data.message));
+    apiRequest()
+  },[])
+
+  return (
+    <div className="App">
+      <h1>フロントエンド</h1>
+      <p>{message}</p>
+    </div>
+  );
+}
+
+export default App;
