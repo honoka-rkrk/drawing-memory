5e4acd31a368e464f3887d81c6f1aea02f0c9547
HONOKA ANRAKU <rakuraku2563nonnon@gmail.com>
2021-10-29 17:19:41 +0900

    addMui and settings cord formatter

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,21 @@
-import React from 'react';
-import './App.css';
-import {useState,useEffect} from 'react';
+import React from "react";
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
-  const [message,setMessage] = useState('');
+  const [message, setMessage] = useState("");
 
   useEffect(() => {
-    const apiRequest = () =>  fetch('/api')
-    .then((res) =>res.json())
-    .then((data) => setMessage(data.message));
-    apiRequest()
-  },[])
+    const apiRequest = () =>
+      fetch("/api")
+        .then((res) => res.json())
+        .then((data) => setMessage(data.message));
+    apiRequest();
+  }, []);
 
   return (
     <div className="App">
-      <h1>フロントエンド</h1>
+      <h1>フロントエンド2</h1>
       <p>{message}</p>
     </div>
   );
---
+++ frontend/src/Style/theme.ts
@@ -0,0 +1,16 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    neutral: {
+      main: "#64748B",
+      second: "#fff",
+    },
+    color2: {
+      main: "#500",
+      second: "#255",
+    },
+  },
+});
+
+export default theme;
